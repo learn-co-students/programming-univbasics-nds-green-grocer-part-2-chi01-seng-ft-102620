200f58fb7bd4ede7f8dcd94f06455077b90c36e3
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-10-14 20:56:58 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,25 +1,62 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  counter = 0
+  while counter < coupons.length
+    item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+    #binding.pry
+    coupon_item = "#{coupons[counter][:item]} W/COUPON"
+    cart_item_coupon = find_item_by_name_in_collection(coupon_item , cart)
+    #binding.pry
+    if item && item[:count] >= coupons[counter][:num]
+      if cart_item_coupon
+        cart_item_coupon[:count] += coupons[counter][:num]
+        item[:count] -= coupons[counter][:num]
+        #binding.pry
+      else
+        cart_item_coupon = {
+            :item => coupon_item,
+            :price => coupons[counter][:cost] / coupons[counter][:num],
+            :clearance => item[:clearance],
+            :count => coupons[counter][:num]
+        }
+        cart << cart_item_coupon
+        item[:count] -= coupons[counter][:num]
+        #binding.pry
+      end
+    end
+    counter +=1
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  counter = 0
+  while counter < cart.length
+    if cart[counter][:clearance]
+      cart[counter][:price] = cart[counter][:price] - (cart[counter][:price] * 0.20)
+    end
+    counter += 1
+  end
+  cart
 end
 
+
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  new_consolidated_cart = consolidate_cart(cart)
+  coupons_cart = apply_coupons(new_consolidated_cart, coupons)
+  receipt = apply_clearance(coupons_cart)
+
+  total = 0
+  counter = 0
+  #binding.pry
+  while counter < receipt.length
+    total += receipt[counter][:price] * receipt[counter][:count]
+    counter += 1
+  end
+  if total > 100
+    total -= (total * 0.10)
+  end
+  total
 end
